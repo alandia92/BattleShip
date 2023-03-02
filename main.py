1f5b8bf46901a6493dbc1e2613c23e41c6e14f55
Arrate <arrate@pop-os.localdomain>
2023-03-02 19:41:22 +0100

    comentarios

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ while disparando:
         print("Acertaste!")
     
     # verificar si el juego a terminado
-    # No se como, igual una funcion nueva
+    # No se como, igual una funcion nueva. disparando seria false
 
     # cambiar al otro jugador
     # igual con pum acierto o no acierto
