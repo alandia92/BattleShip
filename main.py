1c94f9c5dc15c47465a122b84f602f36f8fb5ad4
Arrate <arrate@pop-os.localdomain>
2023-03-02 19:13:47 +0100

    cambiofichero

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+from funciones import*
+from variables import*
+#Hay que hacer algo con los tableros
+disparadno = True
+while disparando:
+    fila = int(input("Elije fila entre 0-9: "))
+    columna = int(input("Elije fila entre 0-9: "))
