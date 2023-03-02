c6d80c357f8dc36ffa47448e87daedeac2f6ea3f
Arrate <arrate@pop-os.localdomain>
2023-03-02 19:27:28 +0100

    notas,dudas

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import numpy as np
 from funciones import*
 from variables import*
-# Hay que hacer algo con los tableros imprimirpor pantalla, no se omo
+# Hay que hacer algo con los tableros imprimirpor pantalla, no se como
+# No se como hacer que cambie de jugador ->  haciendo que no sea 1
+# Pero luego no se como hacer que vuelva a ser otra vez el jugador
 disparando = True
 jugador = 1
 while disparando:
