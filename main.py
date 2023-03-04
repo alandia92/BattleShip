259acfd35228014c5e62a4dac8ca7959da260f00
Arrate Landia <arrate.landia92@gmail.com>
2023-03-04 11:31:48 +0100

    comentarios main y varibles

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from variables import*
 
 while partida_continua:
 
-    print("Campo de batalla\n",tablero_ataque_jugador)
-    print("Tu flota\n",tablero_jugador)
+    # Dos tableros
+    print("Campo de batalla\n",tablero_ataque_jugador)# Donde el jugador va a ver su ataque 
+    print("Tu flota\n",tablero_jugador)# Donde el jugador va a ver sus barcos
     print("-------------------------------------------------------------------")
     if disparando == True:
         fila = None
         columna = None
+        # Gestión de errores de los input del jugador
         while fila is None or columna is None:
             try:
                 fila = int(input("Elije fila entre 0-9: "))
@@ -24,14 +26,17 @@ while partida_continua:
                 columna = None
         disparando = dispararJugador(tablero_ataque_jugador,tablero_maquina,fila,columna)
         if disparando == True:
-            vidas_maquina -= 1
+            vidas_maquina -= 1 # Si acierta el jugador se le resta una vida a la maquina
     else:
         fila = np.random.randint(0,10)
         columna = np.random.randint(0,10)
         disparando = dispararMaquina(tablero_ataque_maquina,tablero_jugador,fila,columna)
-        if disparando == False:
-            vidas_jugador -= 1
+        if disparando == False: 
+            vidas_jugador -= 1# Si acierta la maquina se le resta una vida al jugador
+
     print(f"Te quedan {vidas_jugador} vidas y a tu contrincante {vidas_maquina} vidas")
+
+    # Quien gana
     if vidas_jugador == 0:
         partida_continua = False
         print("Has perdido!")
--- variables.py
+++ variables.py
@@ -1,13 +1,18 @@
 from funciones import*
 
+# Creación de cada tablero
 tablero_jugador = crear_tablero()
 tablero_maquina = crear_tablero()
+# Colocación de los barcos
 colocar_barcos(tablero_jugador)
 colocar_barcos(tablero_maquina)
+# Crear el tablero donde se va a atacar
 tablero_ataque_jugador = crear_tablero()
 tablero_ataque_maquina = crear_tablero()
 
+# Disparando gestiona los turnos del jugador y el contrincante. True: jugador, False: maquina
 disparando = True
+# Partida continua para saber si entrar en el bucle de jugar la partida o no.
 partida_continua = True
 
 vidas_jugador = 20
